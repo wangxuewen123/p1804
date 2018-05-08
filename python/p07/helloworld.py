44b9f81b5656dd3e36c6cd3800f8e02de11d971b
wangxuewen123 <1441660268@qq.com>
2018-05-08 10:52:00 +0800

    python的运算

---
+++ python/p07/helloworld.py
@@ -0,0 +1,19 @@
+# wobuahidao
+print('awdawdwawdawdwawdawdwddd')
+print('enma')
+print('I LOVE YOU!')
+print('我喜欢你')
+#
+'''
+CGHVJKJLNKLNL
+
+
+SDF
+GSD
+GS
+S
+
+SG
+SG
+SF'''
+#在财政在
---
+++ python/p07/yunsuan.py
@@ -0,0 +1,15 @@
+
+a=8
+b=3
+result=a*b
+print(result)
+print('result=a*b',result)
+a=3
+b=23
+result=a/b
+print('result=a/b',result)
+c=6
+result=a*b+c
+print('result=a*b+c',result)
+result=a+b+c
+print('result=a+b+c',result)
