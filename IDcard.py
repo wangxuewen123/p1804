b1f49913989388e56bda4232bf1b4e3bf5cf9e64
wangxuewen123 <1441660268@qq.com>
2018-05-09 18:27:55 +0800

    变量

---
+++ IDcard.py
@@ -0,0 +1,15 @@
+
+# 输入 公司 姓名 职位 电话 邮箱
+name=input("请输入你的姓名： ")
+company=input("请输入你所在的公司： ")
+title=input("请输入你干的什么职位： ")
+phone=int(input("你的电话是什么： "))
+youXiang=input("请输入你的邮箱： ")
+# 显示
+print("*" * 50)
+print("姓名 %s "%(name))
+print("公司名称 %s "%(company))
+print("职位 %s "%(title))
+print("电话 %d "%(phone))
+print("邮箱 %s "%(youXiang))
+print("*" * 50)
